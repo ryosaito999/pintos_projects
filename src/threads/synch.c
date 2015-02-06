0021ecd458ee7099b0db8444f0c2dc8b3726fc58
ryosaito420 <rsait001@sledge.cs.ucr.edu>
2015-02-05 19:21:09 -0800

    LOCKING FINALLY WORKS HOLY SHIT

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -199,6 +199,10 @@ lock_acquire (struct lock *lock)
   ASSERT (!intr_context ());
   ASSERT (!lock_held_by_current_thread (lock));
 
+  if(lock-> holder != NULL){
+  		
+  }
+
   sema_down (&lock->semaphore);
   lock->holder = thread_current ();
 }
