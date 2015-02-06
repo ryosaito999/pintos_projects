5ec1923ad40a1d7d7996d76e92b466035b0d4fd3
ryosaito420 <rsait001@sledge.cs.ucr.edu>
2015-02-06 15:17:27 -0800

    added some comments to our new code

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -68,7 +68,7 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      //list_push_back (&sema->waiters, &thread_current ()->elem);
+      //order semaphore wait list by priority
       list_insert_ordered(&sema->waiters, &thread_current ()->elem, priority_high_low, NULL);
       thread_block ();
     }
@@ -199,6 +199,8 @@ lock_acquire (struct lock *lock)
 
   		
   intr_disable();
+
+  //need to check if lock is already held by a different thread
   if(lock->holder != NULL) {
   		if(lock->holder->priority < thread_current()->priority ){
   			thread_donate_priority(lock->holder);
